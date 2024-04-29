ced7868a958dafc1573fa9651afa7ffaddfad44b
Harsh Ugale <hugale007@gmail.com>
2024-04-29 23:45:58 +0530

    Project Structure

---
+++ app.py
@@ -0,0 +1,13 @@
+from scr.mlprojectds.logger import logging
+from scr.mlprojectds.exception import CustomException
+import sys
+
+
+if __name__ +"__main__":
+    logging.info("The execution has started")
+    
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
