335d62ee9c10b918576654eda85e3bc4cb639f50
Harsh Ugale <hugale007@gmail.com>
2024-05-03 12:19:53 +0530

    data transformation

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from scr.mlprojectds.logger import logging
 from scr.mlprojectds.exception import CustomException
 from scr.mlprojectds.components.data_ingestion import DataIngestion
 from scr.mlprojectds.components.data_ingestion import DataIngestionConfig
+from scr.mlprojectds.components.data_transformation import DataTransformationConfig
+from scr.mlprojectds.components.data_transformation import DataTransformationConfig,DataTransformation
 import sys
 
 
@@ -9,9 +11,13 @@ if __name__ +"__main__":
     logging.info("The execution has started")
     
     try:
-        data_ingestion_config = DataIngestionConfig()
+        # data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        
+        # data_transformation_config=DataTransformation()
+        data_transformation=DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
         
         
     except Exception as e:
